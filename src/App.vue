1e689069be81911e47a60dccb6e505dc11e593cf
AgoDe <ago.defelice@gmail.com>
2022-01-27 15:38:45 +0100

    stamp: header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
     
+      <header-container/>
+
   </div>
 </template>
 
 <script>
-
+import HeaderContainer from './components/HeaderContainer.vue'
 
 export default {
   name: 'App',
   components: {
+    HeaderContainer,
     
   }
 }
 </script>
 
 <style lang="scss">
-
+  @import '@/styles/main.scss'
 </style>
---
+++ src/components/HeaderContainer.vue
@@ -0,0 +1,69 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="logo">
+        </div>
+
+        <nav>
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </nav>
+
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables.scss';
+
+    header {
+        margin: 0 auto;
+        height: 80px;
+        width: 900px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        
+        .logo {
+            img {
+                height: 60px;
+            }
+        }
+
+        ul {
+            display: flex;
+            align-items: center;
+            list-style: none;
+
+            li {
+                font-size: 13px;
+                padding:  0 15px;
+                height: 100%;
+                line-height: 80px;
+                border-bottom: 4px transparent solid;
+
+                &:hover {
+                    border-color: $tipically-blue;
+                    
+                }
+
+            }
+        }
+    }
+
+</style>
