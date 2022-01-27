32e6b515989a97dd4a0fa57eff9a2f45c09063af
AgoDe <ago.defelice@gmail.com>
2022-01-27 15:44:48 +0100

    stamp: favicon

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang"scss" scoped>
+    @import '@/styles/variables.scss';
+    
+</style>
